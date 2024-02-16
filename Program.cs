7ad70d19607e136388df84085df8186e1ebcec9f
The Otterly Side <47543964+TheOtterlySide@users.noreply.github.com>
2024-02-16 23:28:33 +0100

    add: Language Support

--- Program.cs
+++ Program.cs
@@ -1,11 +1,36 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Arbeitszeitenrechner;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Localization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "LanguageFiles");
+builder.Services.AddScoped<IStringLocalizer<Index>,StringLocalizer<Index>>();
+var supportedCultures = new List<CultureInfo> 
+    { 
+        new CultureInfo("en"), 
+        new CultureInfo("de") 
+    };
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("de");
+    options.SupportedUICultures = supportedCultures;
+});
+
+var culture = new CultureInfo("de");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+
 
 await builder.Build().RunAsync();
