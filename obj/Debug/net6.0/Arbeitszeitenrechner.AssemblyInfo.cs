c3ce7e0201ae971c0b75ec180f2cef306a461031
The Otterly Side <47543964+TheOtterlySide@users.noreply.github.com>
2025-05-23 15:32:03 +0200

    fix: Tab Change triggered calculate time

--- obj/Debug/net6.0/Arbeitszeitenrechner.AssemblyInfo.cs
+++ obj/Debug/net6.0/Arbeitszeitenrechner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arbeitszeitenrechner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d2294bf6f610e290508b0f8131c88e1216de2b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e75eaf3c4227e247f922a348dbe32a99d68353c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arbeitszeitenrechner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arbeitszeitenrechner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
