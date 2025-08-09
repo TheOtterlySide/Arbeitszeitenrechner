5fe9e5f4b64a70b6d2278a6098401667b76bed87
The Otterly Side <47543964+TheOtterlySide@users.noreply.github.com>
2025-08-10 00:15:25 +0200

    add: Nightmode + Net 9.0 Update

--- wwwroot/scripts/main.js
+++ wwwroot/scripts/main.js
@@ -14,6 +14,40 @@ function isVisible() {
 
 }
 
+window.themeManager = {
+    init: function () {
+        // Letzte Einstellung aus LocalStorage laden
+        const storedTheme = localStorage.getItem('theme');
+        if (storedTheme) {
+            document.documentElement.setAttribute('data-bs-theme', storedTheme);
+        }
+    },
+    toggle: function () {
+        const html = document.documentElement;
+        const currentTheme = html.getAttribute("data-bs-theme");
+        const newTheme = currentTheme === "dark" ? "light" : "dark";
+        html.setAttribute("data-bs-theme", newTheme);
+        localStorage.setItem('theme', newTheme);
+    }
+};
+
+window.initThemeToggle = () => {
+    const btn = document.getElementById("themeToggle");
+    if (!btn) return; // Falls Button nicht da ist
+
+    btn.addEventListener("click", function () {
+        document.body.classList.toggle("dark-mode");
+
+        const icon = this.querySelector("i");
+        if (document.body.classList.contains("dark-mode")) {
+            icon.classList.replace("bi-moon-stars", "bi-brightness-high");
+        } else {
+            icon.classList.replace("bi-brightness-high", "bi-moon-stars");
+        }
+    });
+};
+
+
 function getVisibleState() {
     return visibleStateofTab;
 }
